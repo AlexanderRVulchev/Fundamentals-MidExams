d6d8617a7bd44691334b0bd285fc132cc2090680
AlexanderRVulchev <106471266+AlexanderRVulchev@users.noreply.github.com>
2022-06-21 18:41:21 +0300

    Add files via upload

---
+++ 06. Programming Fundamentals Mid Exam Retake/01. Black Flag/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+
+internal class Program
+{
+    static void Main()
+    {
+        int totalDays = int.Parse(Console.ReadLine());
+        double dailyPlunder = double.Parse(Console.ReadLine());
+        double expectedPlunder = double.Parse(Console.ReadLine());
+        double plunder = 0;
+
+        for (int day = 1; day <= totalDays; day++)
+        {
+            plunder += dailyPlunder;
+            if (day % 3 == 0) plunder += 0.5 * dailyPlunder;
+            if (day % 5 == 0) plunder *= 0.7;
+        }
+
+        Console.WriteLine(plunder >= expectedPlunder ?
+            $"Ahoy! {plunder:f2} plunder gained." :
+            $"Collected only {plunder / expectedPlunder * 100:f2}% of the plunder.");
+    }
+}
